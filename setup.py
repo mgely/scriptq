024ae2e0aea4c6fc7a2e9516b79e5a1abd96cd5e
Mario Gely - TNW <mfgely@tudelft.net>
2020-04-21 10:35:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="scriptq",
-    version="1.0",
+    version="1.1",
     author="Mario Gely",
     author_email="mario.f.gely@gmail.com",
     description="Python script queuer",
